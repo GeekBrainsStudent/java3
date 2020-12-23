68cc4f5a36084902a9fe85b917063c41b63ab194
GeekBrainsStudent <kvazzz84@gmail.com>
2020-12-23 14:12:40 +0900

    lesson5

--- lesson5/src/main/java/MainClass.java
+++ lesson5/src/main/java/MainClass.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
 
     public static final int CARS_COUNT = 4;
@@ -6,18 +10,38 @@ public class MainClass {
 
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        // Для одновременного старта и для вывода сообщения о начале гонки, вводим объект CyclicBarrier
+        final CyclicBarrier waitStart = new CyclicBarrier(CARS_COUNT,
+                () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
+
+        // Для запрета одновременного заезда в тоннель больше половины машин, вводим семафор
+        final Semaphore tunnelStop = new Semaphore((int) CARS_COUNT/2);
+
+        // Для вывода объявления об окончании гонки вводим объект CountDownLatch
+        final CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+
+        // Передаем Semaphore в консруктор Tunnel
+        Race race = new Race(new Road(60), new Tunnel(tunnelStop), new Road(40));
+
 
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+
+            // Передаем CyclicBarrier и CountDownLatch в конструктор Car
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), waitStart, finish);
         }
 
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
 
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        // ожидаем завершения гонки всеми учатниками
+        try {
+            finish.await();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- lesson5/src/main/java/Tunnel.java
+++ lesson5/src/main/java/Tunnel.java
@@ -1,8 +1,16 @@
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
 
-    public Tunnel() {
+    // Объявляем Semaphore
+    private final Semaphore stop;
+
+    public Tunnel(Semaphore tunnelStop) {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
+
+        // Получаем Semaphore
+        stop = tunnelStop;
     }
 
     @Override
@@ -10,12 +18,17 @@ public class Tunnel extends Stage {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+
+                // Определяем можем ли войти в тоннель
+                stop.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
+                // при выходе из тоннеля
+                stop.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
