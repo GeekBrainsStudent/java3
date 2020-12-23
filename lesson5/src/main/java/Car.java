8c5082ebcd6d4fa14dadcea449f97ea6148b3fa3
GeekBrainsStudent <kvazzz84@gmail.com>
2020-12-23 15:00:53 +0900

    fin lesson5

--- lesson5/src/main/java/Car.java
+++ lesson5/src/main/java/Car.java
@@ -1,6 +1,15 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
 
     private static int CARS_COUNT;
+
+    // Объявляем объект CyclicBarrier
+    private final CyclicBarrier waitStart;
+
+    // Объявляем объект CountDownLatch
+    private final CountDownLatch finish;
 
     static {
         CARS_COUNT = 0;
@@ -18,11 +27,17 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier waitStart, CountDownLatch finish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+
+        // получаем CyclicBarrier с MainClass
+        this.waitStart = waitStart;
+
+        // получаем CountDownLatch с MainClass
+        this.finish = finish;
     }
 
     @Override
@@ -31,11 +46,24 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+
+            // ждем, когда все будут готовы
+            waitStart.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+        } finally {
+//            Участник финишировал
+            finish.countDown();
+
+//            Если участник победил
+            if(finish.getCount() == CARS_COUNT - 1)
+//                Печатаем его имя
+                System.out.println(this.name + " - WIN");
         }
     }
 }
